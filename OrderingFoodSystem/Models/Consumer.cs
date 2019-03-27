17ee6a73a2eff904165abbf02504a62f6dc9bee7
Sadman Ali Mishu <sadman.mishu@gmail.com>
2019-03-27 23:14:16 +0600

    First Commit

---
+++ OrderingFoodSystem/Models/Consumer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace OrderingFoodSystem.Models
+{
+	public class Consumer
+	{
+		[Key]
+		public int CustomerID { get; set; }
+		public string Name { get; set; }
+		public string Phonenumber { get; set; }
+		public string Email { get; set; }
+		public string DeliveryAddress { get; set; }
+		public string Password { get; set; }
+	}
+
+	public class ConsumerDBContext : DbContext
+	{
+		public DbSet<Consumer> Consumers { get; set; }
+	}
+
+	
+
+
+}
---
+++ OrderingFoodSystem/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OrderingFoodSystem.Models
+{
+	public class Customer
+	{
+		public int CustomerID { get; set; }
+		public string Name { get; set; }
+		public string phonenumber { get; set; }
+		public string Email { get; set; }
+		public string DeliveryAddress { get; set; }
+		//public string Password { get; set; }
+	}
+
+	public class CustomerDBContext: DbContext
+	{
+		public DbSet<Customer> Customers { get; set; }
+	}
+}
